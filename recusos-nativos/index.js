9dee702e00e90fda58258f575eb55f71538f845d
Tailan Souza <tailansouza.dev@gmail.com>
2024-01-09 20:48:36 -0300

    recurços basicos

---
+++ recusos-nativos/index.js
@@ -0,0 +1,9 @@
+let nome = "tailan"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+//window.alert(nome)
+//nome = window.prompt("Qual seu nome?")
+let comfirmaçao = window.confirm("deseja continuar")
