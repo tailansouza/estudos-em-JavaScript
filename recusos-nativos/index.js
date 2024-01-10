be0223deaccee38dd3896bfc4bdf29535dbc1e04
Tailan Souza <tailansouza.dev@gmail.com>
2024-01-10 06:46:33 -0300

    estudo Js

--- recusos-nativos/index.js
+++ recusos-nativos/index.js
@@ -1,9 +1,21 @@
-let nome = "tailan"
+//let nome = "tailan"
 
-console.log(nome)
-console.error(nome)
-console.warn(nome)
+//console.log(nome)
+//console.error(nome)
+//ttconsole.warn(nome)
 
 //window.alert(nome)
 //nome = window.prompt("Qual seu nome?")
-let comfirmaçao = window.confirm("deseja continuar")
+//let comfirmaçao = window.confirm("deseja continuar")
+
+//ex1
+let atual = parseFloat(2023)
+let nome = window.prompt("Qual seu nome?")
+let sobre = window.prompt("E seu sobre nome?")
+let estudo = window.prompt("Oque você estuda?")
+let data = window.prompt("Qual seu ano nacimento")
+let ano= parseFloat(atual - data)
+window.alert(
+  "seu nome "+ nome + " seu sobre nome "+ sobre + " voce estuda "+ estudo + " naseu em "+data +" sua idade é " + ano
+)
+
