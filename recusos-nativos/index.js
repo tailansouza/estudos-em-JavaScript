74d28d641645c60e6e009d9b17b31369115786d4
Tailan Souza <tailansouza.dev@gmail.com>
2024-01-10 20:44:45 -0300

    estudo

--- recusos-nativos/index.js
+++ recusos-nativos/index.js
@@ -9,7 +9,7 @@
 //let comfirmaçao = window.confirm("deseja continuar")
 
 //ex1
-let atual = parseFloat(2023)
+/*let atual = parseFloat(2023)
 let nome = window.prompt("Qual seu nome?")
 let sobre = window.prompt("E seu sobre nome?")
 let estudo = window.prompt("Oque você estuda?")
@@ -18,4 +18,24 @@ let ano= parseFloat(atual - data)
 window.alert(
   "seu nome "+ nome + " seu sobre nome "+ sobre + " voce estuda "+ estudo + " naseu em "+data +" sua idade é " + ano
 )
+*/
+let  num1 = window.prompt ("digite o primeiro numero:")
+let num2 = window.prompt("escolha mais um numero:")
+let n1 = parseFloat(num1)
+let  n2 = parseFloat(num2)
+
+let soma = n1+n2
+let sub = n1-n2
+let muti = n1*n2
+let div = n1/n2
+
+alert(
+  "o resutado foi : \n"+
+  "\n soma : "+soma+
+  "\n subtração : "+sub+
+  "\n mutipliucação : "+muti+
+  "\n divisão : "+div+
+  "\n obrigado"
+)
+
 
