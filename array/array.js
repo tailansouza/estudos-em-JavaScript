8fbe67a4f13d3b1946d4eca469c4d020bfd03604
Tailan Souza <tailansouza.dev@gmail.com>
2024-01-23 20:19:30 -0300

    array

---
+++ array/array.js
@@ -0,0 +1,25 @@
+/*const shoppingList = []
+
+shoppingList[0]="\nbacalhal"
+shoppingList[1]="\npimenta"
+shoppingList[2]="\nacerola"
+
+alert(shoppingList)*/
+
+//trabalhando com arrays
+
+const arr =["Frodo", "Sam" , "Marry" , "Pippin" ,"Gandalf" , "Aragorn" , "Legolas" , "Gimli"]
+
+//adicionar elementos : push
+arr.push("Utimo")
+//unshift
+arr.unshift("primeiro")
+//pesquisa de elemento: includes
+const inclui = arr.includes("Gandalf")
+
+//cortar e contatenar
+//slice
+const hobbits = arr.slice(0, 4)
+
+
+alert(arr + "\n" +inclui +"\n" + hobbits)
